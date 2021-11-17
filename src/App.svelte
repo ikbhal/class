01c72f103618318c89b85a95a9289ac61ee4f7b0
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-17 12:54:08 +0530

    adding process commans

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,73 @@
 <script>
-	export let name;
+let cmd = "";
+let messages = [];
+function cmdHandler(e) {
+	console.log("cmd handler e", e);
+	if(e.key=="Enter"){
+		processCmd(cmd);
+	}
+}
+
+function processCmd(c) {
+	console.log("process cmd c:",c);
+	if(c.startsWith('/')){
+		c = c.substring(1);
+	}
+	let parts = c.split(" ");
+	let cmdName = '';
+	if(parts && parts.length > 0){
+		cmd = parts[0];
+	}
+	cmdName = cmdName.toLowerCase();
+	switch(cmdName){
+		case 'youtube':
+			console.log("youtube video");
+			break;
+		case "tutorial":
+			console.log("tutorial link, which language , framework, topic, category , tags");
+			break;
+		case 'img':
+		case 'image':
+			console.log("image handling");
+			break;
+		default:
+			textCommand(c);
+	}
+}
+
+function textCommand(c){
+	console.log("inside text command");
+	appendMessage(c);
+}
+
+function appendMessage(msg){
+	messages.push(msg);
+}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Class</h1>
+	
+	<p>slack commands (tutorial, link , chat, youotube vide, image, screen shot,  github, vercel)</p>
+	<p>class is considered as place, may be subject wise</p>
+	
+	<hr/>
+	<form on:submit|preventDefault={() => console.log("form submitted")}>
+		<input type="text" nam="cmd" bind:value={cmd} on:keyup={cmdHandler}>
+	</form>
+	<hr/>
+	<div class="output">
+		cmd: {cmd}
+	</div>
+	<div class="chat">
+		{#each messages as msg}
+		<div class="messsage">
+		{msg}
+		</div>
+		{/each}
+
+	</div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
